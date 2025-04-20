ce6464cc1dd59f40ab2f9628d3226c6e19e170ae
Radhey Shyam <radheyyt2004@gmail.com>
2025-04-20 17:10:16 +0000

    User Input

---
+++ userInput.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cout << "Type a number: " << "\n";
+    cin >> num;
+    cout << "Your number is: " << num << "\n";
+    return 0;
+}
