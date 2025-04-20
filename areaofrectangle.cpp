d3d96eee357ddd1258f644f6e47f62cf529aa451
Radhey Shyam <radheyyt2004@gmail.com>
2025-04-20 17:02:12 +0000

    Area of Rectangle

---
+++ areaofrectangle.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int w = 12;
+    int h = 13;
+
+    int area = w*h;
+    cout << "Area of Rectangle is " << area << " meter";
+    return 0;
+}
