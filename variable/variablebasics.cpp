39df0eac086d942c99192afcc83e3dc15ecc5f18
Radhey Shyam <radheyyt2004@gmail.com>
2025-04-20 16:44:19 +0000

    Var

---
+++ variable/variablebasics.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int num = 12;
+    int n;
+    n = 100;
+    double doub = 12.67;
+    char a= 'a';
+    string name = "Radhey Shyam";
+    bool mybool = true;
+    cout << num << "\n";
+    cout << n << "\n";
+    cout << name << "\n";
+    cout << mybool << "\n";
+}
