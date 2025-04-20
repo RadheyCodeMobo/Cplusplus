f9ef14b0b59179d4533c5324ea20b7f02e402c8f
Radhey Shyam <radheyyt2004@gmail.com>
2025-04-20 16:43:57 +0000

    variable

--- newline.cpp
+++ newline.cpp
@@ -4,5 +4,16 @@ using namespace std;
 int main(){
     cout << "Radhey \n";
     cout << "Shyam";
+
+    cout << "Radhey" << "\n";
+    cout << "Shyam";
+
+    cout << "Radhey" << "\n\n"; //new blank line
+    cout << "Shyam";
+    
+    cout << "Radhey" << endl;
+    cout << "Shyam";
+
+
     return 0;
 }
